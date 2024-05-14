d50d569fed1f83d5ece263418d6dcec8c648779f
신지연 <shinjy@sinjiyeon-ui-MacBookAir.local>
2024-05-14 16:54:07 +0900

    Setting: 초기 세팅

---
+++ WorldWordWeb/WordWideWeb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldWordWeb
+//
+//  Created by 신지연 on 2024/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
