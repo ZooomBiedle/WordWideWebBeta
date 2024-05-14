794218d58835983bc917a0ca89b91529d79b4ae4
mgynsz <mgynsz@gmail.com>
2024-05-14 17:34:53 +0900

    Setting: 초기세팅3

--- WordWideWeb/WordWideWeb/ViewController.swift
+++ WordWideWeb/WordWideWeb/ViewController.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.white
+        setupViews()
+    }
+    
+    private func setupViews() {
+        titleLabel.text = "WWW"
+        
+        view.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.centerY.equalTo(view)
+        }
     }
 
 
