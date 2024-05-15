1a0e25737af7ed79f881f15123651c2240818477
우유차 <142779268+labydin@users.noreply.github.com>
2024-05-15 18:45:29 +0900

    Setting: 재업로드

---
+++ WordWideWeb/WordWideWeb/Model/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  WorldWordWeb
+//
+//  Created by 신지연 on 2024/05/14.
+//
+
+import Foundation
+
+final class NetworkManager {
+    // MARK: - properties
+    
+    // MARK: - methods
+    
+}
